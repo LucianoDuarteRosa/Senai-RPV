a4f25d0e3770d0f498653c4ffc0d9e2c4f1fed71
Luciano Duarte <lucianoduarterosa@hotmail.com>
2024-01-29 22:16:12 -0300

    Atividade 3

---
+++ Exercicio3.js
@@ -0,0 +1,33 @@
+let historicoAluno = {
+    ensinoMedio: true,
+    ensinoSuperior: true,
+    hardSkils: ['adapatabilidade', 'flexível', 'dedicado'],
+    idioma: ['ingles', 'espanhol', 'russo']
+}
+
+let curso = {
+    nomeCursoConcluido: 'POO', 
+    anoDeConclusao: '2019'}
+
+
+let cursoProfissional = {
+    nomeAluno: 'Larissa',
+    nomeCurso: 'FullStack',
+    valorCurso: 95,
+    infoCurso: ['manhã', 'tarde', 'noite'],
+    cursoConclusao:[curso],
+    historico: historicoAluno
+}
+
+
+console.log(cursoProfissional.cursoConclusao[0].nomeCursoConcluido);
+console.log(cursoProfissional.cursoConclusao[0].anoDeConclusao);
+console.log(cursoProfissional.historico.ensinoMedio);
+console.log(cursoProfissional.historico.ensinoSuperior);
+console.log(cursoProfissional.historico.hardSkils);
+console.log(cursoProfissional.historico.idioma);
+
+
+
+
+
