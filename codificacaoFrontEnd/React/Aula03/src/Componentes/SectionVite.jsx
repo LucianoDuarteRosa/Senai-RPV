33561b9a9dd223d5bf0d856a6a241be433fb8b9b
Luciano Duarte <lucianoduarterosa@hotmail.com>
2024-04-04 22:55:06 -0300

    atividade pratica de react

---
+++ codificacaoFrontEnd/React/Aula03/src/Componentes/SectionVite.jsx
@@ -0,0 +1,13 @@
+import style from './SectionVite.module.css'
+
+export function SectionVite(){
+
+    return(
+        <section className={style.sectionVite}>
+            <h1>Um pouco sobre mim</h1>
+            <p>Após dedicar um extenso período ao comércio, onde adquiri valiosos insights sobre gestão de pessoas no contexto do atendimento a clientes e
+            colaboradores, percebi a oportunidade de expandir meus horizontes profissionais. Durante minha trajetória,transitei por diversos setores, incluindo
+            compras, financeiro e fiscal, o que me proporcionou uma ampla gama de conhecimentos em áreas diversas. Com o intuito de migrar para uma nova carreira, iniciei meus estudos em Java e, atualmente, estou concentrando meus esforços em aprofundar minha expertise em C#. Estou absorvendo conceitos fundamentais de Orientação a Objetos, coleções, UML, ASP.NET, Entity Framework, LINQ e Lambda, visando desenvolver uma base sólida e   relevante para os desafios futuros.</p>
+        </section>
+    )
+}
